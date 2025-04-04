44a002026e47cd4b51dd0b25cb4a262939a7b329
Daniel <gulnarikmihailova@gmail.com>
2025-04-04 03:00:05 +0300

    на проверку

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
-HTML_DIR = BASE_DIR / 'html'
+HTML_DIR = BASE_DIR / "html"
--- src/main.py
+++ src/main.py
@@ -1,33 +1,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 from src.config import HTML_DIR
 
 hostName = "localhost"
 serverPort = 8080
 
+
 class MyServer(BaseHTTPRequestHandler):
     """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
     """
 
-    def reading_html(self, page_name: str) -> str:
-        """ Возвращает страницу html, определенную параметром page_name. """
-        with open(HTML_DIR / page_name, encoding='utf-8') as html_file:
+    def html_page(self, page_name: str) -> str:
+        """Возвращает страницу html, определенную параметром page_name."""
+        with open(HTML_DIR / page_name, encoding="utf-8") as html_file:
             html_file = html_file.read()
         return html_file
 
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов """
-        if self.path == '/' or self.path == '/glavnaya.html':
-            page_content = self.reading_html('glavnaya.html')
-        else:
-            page_content = self.reading_html('contacts.html')
+        """Метод для обработки входящих GET-запросов"""
+        page_content = self.html_page("contacts.html")
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.send_header(
+            "Content-type", "text/html"
+        )  # Отправка типа данных, который будет передаваться
         self.end_headers()
 
-        self.wfile.write(bytes(page_content, 'utf-8'))
+        self.wfile.write(bytes(page_content, "utf-8"))
 
 
 if __name__ == "__main__":
